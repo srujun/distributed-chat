445d83e7db73c08f987b05db48920e54bc232697
sgupta80 <sgupta80@illinois.edu>
2017-02-06 15:38:07 -0600

    Added ChatUI class.

---
+++ ChatUI.py
@@ -0,0 +1,59 @@
+import curses
+
+class ChatInterface:
+    def __init__(self, stdscr, chatbox_lines=2):
+        stdscr.clear()
+        stdscr.scrollok(True)
+        curses.use_default_colors()
+
+        # nlines, ncols, begin_y, begin_x
+        win_messages_props = [curses.LINES - chatbox_lines, curses.COLS, 0, 0]
+        win_chatbox_props = [chatbox_lines, curses.COLS,
+                             curses.LINES - chatbox_lines, 0]
+
+        self.win_messages = stdscr.derwin(*win_messages_props)
+        self.win_messages.scrollok(True)
+        self.win_chatbox = stdscr.derwin(*win_chatbox_props)
+        self.stdscr = stdscr
+
+        self.chatbuffer = []
+
+        self.stdscr.refresh()
+
+    def redraw_chatbox(self):
+        self.win_chatbox.clear()
+        message = ''.join(self.chatbuffer)
+        self.win_chatbox.addstr(0, 0, message)
+        self.win_chatbox.refresh()
+
+    def get_input(self, prompt=''):
+        self.chatbuffer.clear()
+        self.chatbuffer.append(prompt)
+        self.redraw_chatbox()
+
+        while True:
+            inch = self.win_chatbox.getch()
+            xpos = self.win_chatbox.getyx()[1]
+
+            if inch == curses.KEY_BACKSPACE or inch == 127:
+                if len(self.chatbuffer) - 1 > 0:
+                    self.chatbuffer.pop()
+            elif inch == ord('\n'):
+                message = ''.join(self.chatbuffer[1:])
+                self.chatbuffer.clear()
+                return message
+            elif xpos >= self.win_chatbox.getmaxyx()[1] - 2:
+                continue
+            else:
+                self.chatbuffer.append(chr(inch))
+
+            self.redraw_chatbox()
+
+    def add_message(self, message, username=''):
+        if not message.endswith('\n'):
+            message = message + '\n'
+
+        if username:
+            username = username + ": "
+        self.win_messages.addstr(username + message)
+        self.win_messages.refresh()
