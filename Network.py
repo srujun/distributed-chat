ef8a29a1cb7f4385203439dc08fd1059ad07ae21
sgupta80 <sgupta80@illinois.edu>
2017-02-27 22:53:07 -0600

    HUGE commit with locks and stuff

--- Network.py
+++ Network.py
@@ -17,13 +17,18 @@ class Network:
         # logging.basicConfig(filename='network.log',level=logging.DEBUG)
 
         self.nodelist = nodelist
-        # map from host ip (str) -> [socket, port]
+        # map from host ip (str) -> socket
         self.alive = {}
+        self.alive_mutex = threading.Lock()
 
         # counter used for ISIS ordering
         self.counter = 0
+        self.counter_mutex = threading.Lock()
+
         # message queue for ISIS ordering
         self.msgqueue = []
+        self.queue_mutex = threading.Lock()
+
         # unique identifier for appending to each priority number
         # it is the sum of the digitis of the IP address
         self.uid = reduce(add, map(int, Network.get_ip().split('.')))
@@ -51,7 +56,10 @@ class Network:
                 pass
             else:
                 logging.debug('Connection successful!')
+
+                self.alive_mutex.acquire()
                 self.alive[socket.gethostbyname(node)] = sock
+                self.alive_mutex.release()
 
         logging.debug('# alive = ' + str(len(self.alive)))
         logging.debug(str(self.alive))
@@ -82,7 +90,10 @@ class Network:
             else:
                 ip, port = addr
                 logging.debug('Got connection from ' + ip)
-                self.alive[ip] = [clientsocket, port]
+
+                self.alive_mutex.acquire()
+                self.alive[ip] = clientsocket
+                self.alive_mutex.release()
                 self.start_receivers(receivers=[ip])
 
 
@@ -97,26 +108,67 @@ class Network:
             receiver.start()
 
 
+    def recv_msg(self, host, callback):
+        while True:
+            try:
+                self.alive_mutex.acquire()
+                pickled = self.alive[host].recv(2048)
+
+                if not pickled:
+                    self.alive_mutex.release()
+                    self.handle_crash(host)
+                    break
+
+                self.alive_mutex.release()
+
+                message = pickle.loads(pickled)
+                if not isinstance(message, Message):
+                    logging.warning('Unpickling received msg unsuccessful')
+                else:
+                    self.handle_message(message)
+                time.sleep(0.5)
+
+            except socket.timeout:
+                time.sleep(1)
+                continue
+
+
     def bcast_msg(self, msg, destinations=[], wait=True):
         threads = []
+        is_all_hosts = False
 
         if len(destinations) == 0:
+            is_all_hosts = True
+            self.alive_mutex.acquire()
             destinations = self.alive.keys()
 
         # add msg to msgqueue if it is a new chat message
         if msg.msgtype == Message.CHAT:
             qmsg = copy.deepcopy(msg)
+
+            self.counter_mutex.acquire()
             self.counter += 1
-            qmsg.final = Network.merge_float(self.counter, self.uid)
+            logging.debug('Incrementing counter to {}'.format(self.counter))
+            qmsg.priority = Network.merge_float(self.counter, self.uid)
+            self.counter_mutex.release()
+
             qmsg.deliverable = False
+            qmsg.alive_set = set(self.alive.keys())
+
+            self.queue_mutex.acquire()
             self.msgqueue.append(qmsg)
-            self.msgqueue.sort(key=lambda m: m.final)
+            self.msgqueue.sort(key=lambda m: m.priority)
+            self.queue_mutex.release()
+
             logging.debug('Queue: {}'.format(self.msgqueue))
 
         for host in destinations:
             t = threading.Thread(target=self.send_msg, args=(msg, host))
             threads.append(t)
             t.start()
+
+        if is_all_hosts:
+            self.alive_mutex.release()
 
         if wait:
             for thread in threads:
@@ -129,56 +181,23 @@ class Network:
         pickled = pickle.dumps(msg)
 
         totalsent = 0
+        self.alive_mutex.acquire()
+
         while totalsent < len(pickled):
             try:
                 sent = self.alive[host].send(pickled[totalsent:])
             except socket.error:
-                # TODO: show offline at the right time
-                callback(host + " went offline...")
-                del self.alive[host]
-                logging.debug(host + ' went offline...')
-                break
-            except KeyError:
-                # TODO: the host has been removed
-                callback(host + " went offline...")
-                logging.debug(host + ' went offline...')
+                self.alive_mutex.release()
+                self.handle_crash(host)
                 break
             if sent == 0:
-                # lost connection
                 logging.debug('Could not send msg, lost connection!')
-                del self.alive[host]
+                self.alive_mutex.release()
+                self.handle_crash(host)
                 break
 
             totalsent += sent
-
-
-    def recv_msg(self, host, callback):
-        while True:
-            try:
-                pickled = self.alive[host].recv(1024)
-                if not pickled:
-                    # TODO: show offline at the right time
-                    callback(host + " went offline...")
-                    del self.alive[host]
-                    logging.debug(host + ' went offline...')
-                    break
-
-                message = pickle.loads(pickled)
-                if not isinstance(message, Message):
-                    logging.warning('Unpickling received msg unsuccessful ' + \
-                                    str(type(message)))
-                else:
-                    callback(message)
-                time.sleep(0.5)
-
-            except socket.timeout:
-                time.sleep(1)
-                continue
-            except KeyError:
-                # TODO: the host has been removed
-                callback(host + " went offline...")
-                logging.debug(host + ' went offline...')
-                break
+        self.alive_mutex.release()
 
 
     def handle_message(self, message):
@@ -191,65 +210,183 @@ class Network:
             return
 
         # need to respond with a proposed priority
-        if message.msgtype == Message.CHAT and \
-                (message.proposed < 0 and message.final < 0):
+        if message.msgtype == Message.CHAT:
+            logging.debug('Got NEW msg')
             proposal = Message(Message.PROPOSAL, Network.get_ip(),
                                msgid=message.msgid)
+            self.counter_mutex.acquire()
             self.counter += 1
-            proposal.proposed = Network.merge_float(self.counter, self.uid)
+            logging.debug('Incrementing counter to {}'.format(self.counter))
+            proposal.priority = Network.merge_float(self.counter, self.uid)
+            self.counter_mutex.release()
+
+            message.priority = proposal.priority
+            message.deliverable = False
 
             # store msg in queue with the proposed priority
-            message.final = proposal.proposed
-            message.deliverable = False
+            self.queue_mutex.acquire()
             self.msgqueue.append(message)
-            self.msgqueue.sort(key=lambda m: m.final)
-            logging.debug('Got NEW msg. Queue: {}'.format(self.msgqueue))
+            self.msgqueue.sort(key=lambda m: m.priority)
+            self.queue_mutex.release()
+            logging.debug('Queue: {}'.format(self.msgqueue))
 
+            # send the proposal back to the sender
             self.bcast_msg(proposal, destinations=[message.origin], wait=False)
 
         # wait until we receive proposals from everyone
         elif message.msgtype == Message.PROPOSAL:
             logging.debug('Got proposal')
 
-            # get the original mssage from the queue
-            orig = next(m for m in self.msgqueue if m.msgid == message.msgid)
-            try:
-                orig.proposals.append(message)
-            except AttributeError:
-                orig.proposals = [ message ]
+            # get the original msg from the queue
+            self.queue_mutex.acquire()
+            orig = next([m for m in self.msgqueue if m.msgid == message.msgid],
+                        None)
+            if orig is None:
+                logging.warning('Bogus proposal. ID: {}'.format(message.msgid))
+                self.queue_mutex.release()
+                return
+
+            # add this proposal to the original message's proposals set
+            orig.proposals_mutex.acquire()
+            orig.proposals.add(message)
+            orig.proposals_mutex.release()
+            # update the counter
+            self.counter_mutex.acquire()
+            logging.debug('Counter update from {}'.format(self.counter))
+            self.counter = max(self.counter, message.priority) + 1
+            logging.debug('Counter update to {}'.format(self.counter))
+            self.counter_mutex.release()
 
             # if we have received proposals from everyone, mark as deliverable
             # and remulticast it
-            if len(orig.proposals) == len(self.alive):
+            # we know if everyone has sent proposals if the set of nodes that
+            # have sent a proposal equals the intersection of the set of nodes
+            # alive at send time and the set of nodes alive now
+
+            self.alive_mutex.acquire()
+            alive_rn = set(self.alive.keys())
+            self.alive_mutex.release()
+
+            orig.proposals_mutex.acquire()
+            received_proposals = set([m.origin for m in orig.proposals])
+            orig.proposals_mutex.release()
+
+            if orig.alive_set & alive_rn == received_proposals:
                 orig.deliverable = True
-                orig.final = max(orig.proposals, key=lambda m: m.proposed)
+                orig.priority = max(orig.priority,
+                                max(orig.proposals, key=lambda m: m.priority))
                 logging.debug('Marking deliverable!')
                 logging.debug('Queue: {}'.format(self.msgqueue))
 
+                # multicast msg with final priority
                 final = Message(Message.FINAL, Network.get_ip(),
                                 msgid=orig.msgid)
-                final.final = orig.final
+                final.priority = orig.priority
+                self.queue_mutex.release()
                 self.bcast_msg(final, wait=False)
+            else:
+                self.queue_mutex.release()
 
+        # sender has sent final priority
         elif message.msgtype == Message.FINAL:
             logging.debug('Got final')
 
-        # deliver all msgs at start of queue
+            # get the original msg from the queue
+            self.queue_mutex.acquire()
+            orig = next([m for m in self.msgqueue if m.msgid == message.msgid],
+                        None)
+            if orig is None:
+                logging.warning('Bogus final. ID: {}'.format(message.msgid))
+                self.queue_mutex.release()
+                return
+
+            # update msg final priority
+            orig.priority = message.priority
+            # update the counter
+            self.counter_mutex.acquire()
+            logging.debug('Counter update from {}'.format(self.counter))
+            self.counter = max(self.counter, message.priority) + 1
+            logging.debug('Counter update to {}'.format(self.counter))
+            self.counter_mutex.release()
+
+            # mark as deliverable
+            orig.deliverable = True
+            logging.debug('Marking deliverable!')
+            logging.debug('Queue: {}'.format(self.msgqueue))
+
+            self.queue_mutex.release()
+
+
+    def handle_crash(self, host):
+        logging.debug('{} crashed!'.format(crashed))
+        logging.debug('Removing crashed node from alive list')
+        self.alive_mutex.acquire()
+        # remove crashed node from alive list
+        del self.alive[host]
+        self.alive_mutex.release()
+
+        logging.debug('Acquiring Queue mutex')
+        self.queue_mutex.acquire()
+
+        for msg in self.msgqueue:
+            # msg was originally sent by the crashed node
+            if msg.origin == host:
+                logging.debug('Deleting msg {}'.format(msg))
+                # delete the message (regardless of deliverability)
+                del self.msgqueue[msg]
+
+            # msg was sent by me, waiting for proposal from crashed node
+            elif msg.origin == Network.get_ip():
+                # remove crashed node from proposals set
+                logging.debug('Discarding {} from proposals list'.format(host))
+                msg.proposals_mutex.acquire()
+                msg.proposals.discard(host)
+                received_proposals = set([m.origin for m in msg.proposals])
+                msg.proposals_mutex.release()
+
+                self.alive_mutex.acquire()
+                alive_rn = set(self.alive.keys())
+                self.alive_mutex.release()
+
+                logging.debug('Checking if can be delivered...')
+                # check if this can be delivered
+                if msg.alive_set & alive_rn == received_proposals:
+                    msg.deliverable = True
+                    msg.priority = max(
+                        msg.priority,
+                        max(msg.proposals, key=lambda m: m.priority)
+                    )
+                    logging.debug('Marking deliverable with '
+                                  'prio {}!'.format(msg.priority))
+                    logging.debug('Queue: {}'.format(self.msgqueue))
+
+                    # multicast msg with final priority
+                    final = Message(Message.FINAL, Network.get_ip(),
+                                    msgid=msg.msgid)
+                    final.priority = msg.priority
+                    self.queue_mutex.release()
+                    self.bcast_msg(final, wait=True)
+                    self.queue_mutex.acquire()
+
+        self.queue_mutex.release()
 
 
     def close(self):
+        logging.debug('Closing all sockets')
         for host in self.alive.keys():
             self.alive[host].close()
 
 
 class Message:
     # msgtype can be one of "chat", "proposal", "final"
-    CHAT = "chat"
-    PROPOSAL = "proposal"
-    FINAL = "final"
+    CHAT = 'chat'
+    PROPOSAL = 'proposal'
+    FINAL = 'final'
 
     def __init__(self, msgtype, origin, msgid=None, text='', username=''):
+        # raise TypeError('Message type unknown' + str(msgtype))
         self.msgtype = msgtype
+        self.origin = origin
         if not msgid:
             self.msgid = uuid.uuid1()
         else:
@@ -257,18 +394,19 @@ class Message:
         self.text = text
         self.username = username
 
-        self.origin = origin
-
-        self.proposed = -1
-        self.final = -1
-
+        self.priority = -1
         self.deliverable = False
 
-        # raise TypeError('Message type unknown' + str(msgtype))
+        # set of proposals
+        self.proposals = set()
+        self.proposals_mutex = threading.Lock()
+        # alive list at time of send
+        self.alive_set = set()
+
 
     def __repr__(self):
         fmt = ('Message(type={}, origin={}, id={}, username="{}", text="{}", '
-               'proposed={}, final={}, deliverable={})')
+               'priority={}, deliverable={})')
         return fmt.format(
             self.msgtype, self.origin, self.msgid, self.username, self.text,
-            self.proposed, self.final, self.deliverable)
+            self.priority, self.deliverable)
