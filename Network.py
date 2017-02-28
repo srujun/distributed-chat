f8655167a49a1c544e29babd4323417d4c88136e
sgupta80 <sgupta80@illinois.edu>
2017-02-28 06:33:23 -0600

    Added log after sorting in delivery thread

--- Network.py
+++ Network.py
@@ -425,6 +425,7 @@ class Network:
             time.sleep(0.5)
             self.queue_mutex.acquire()
             self.msgqueue.sort(key=lambda m: m.priority)
+            logging.debug('Sorted Queue: {}'.format(self.msgqueue))
             for i, msg in enumerate(self.msgqueue):
                 if msg.deliverable:
                     logging.debug('Delivering msg {}'.format(msg))
